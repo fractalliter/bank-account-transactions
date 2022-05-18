7031cea0b1d96d5777e4a1ff46237f2715cc9e06
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-18 20:56:54 +0430

    add global exception handler

--- src/main/java/com/tuum/bankassignment/BankAssignmentApplication.java
+++ src/main/java/com/tuum/bankassignment/BankAssignmentApplication.java
@@ -1,5 +1,6 @@
 package com.tuum.bankassignment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -7,7 +8,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true)
 public class BankAssignmentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BankAssignmentApplication.class, args);
     }
