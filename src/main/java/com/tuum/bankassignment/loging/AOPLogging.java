ec433a34d0d96af9e64f6c3a0e9f25307282cd99
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-18 20:54:57 +0430

    add aspect oriented loging

--- src/main/java/com/tuum/bankassignment/loging/AOPLogging.java
+++ src/main/java/com/tuum/bankassignment/loging/AOPLogging.java
@@ -15,8 +15,15 @@ public class AOPLogging {
     @Autowired
     RabbitMQSender rabbitMQSender;
 
-    @AfterReturning(value = "execution(* com.tuum.bankassignment.service.AccountService.createAccount*(..))", returning = "result")
+    @AfterReturning(value = "execution(* com.tuum.bankassignment.service.AccountService.*(..))", returning = "result")
     public void afterCreateAccount(JoinPoint joinPoint, Object result) {
+        System.out.println("After method:" + joinPoint.getSignature());
+        System.out.println("Creating account with first name"+result.toString());
+        rabbitMQSender.send(result);
+    }
+
+    @AfterReturning(value = "execution(* com.tuum.bankassignment.service.TransactionService.*(..))", returning = "result")
+    public void afterCreateTransaction(JoinPoint joinPoint, Object result) {
         System.out.println("After method:" + joinPoint.getSignature());
         System.out.println("Creating account with first name"+result.toString());
         rabbitMQSender.send(result);
