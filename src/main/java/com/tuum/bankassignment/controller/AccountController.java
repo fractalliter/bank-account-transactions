78d345d89cc0dd5cea5b793b6523204b46a896a6
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-18 20:52:16 +0430

    add route to controllers

--- src/main/java/com/tuum/bankassignment/controller/AccountController.java
+++ src/main/java/com/tuum/bankassignment/controller/AccountController.java
@@ -2,26 +2,31 @@ package com.tuum.bankassignment.controller;
 
 import com.tuum.bankassignment.dto.CreateAccountDTO;
 import com.tuum.bankassignment.entity.Account;
+import com.tuum.bankassignment.exception.AccountNotFoundException;
+import com.tuum.bankassignment.exception.InvalidCurrencyException;
 import com.tuum.bankassignment.service.AccountService;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.javassist.NotFoundException;
 import org.apache.ibatis.javassist.tools.web.BadHttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("account")
 public class AccountController {
 
     @Autowired
     AccountService accountService;
 
-    @PostMapping("/account")
-    Account createBankAccount(@RequestBody CreateAccountDTO account) throws NotFoundException, BadHttpRequest {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Account createBankAccount(@RequestBody CreateAccountDTO account) throws InvalidCurrencyException {
         return accountService.createAccount(account);
     }
 
-    @GetMapping("/account/{accountId}")
-    Account getAccount(@PathVariable Long accountId) throws NotFoundException {
+    @GetMapping("{accountId}")
+    Account getAccount(@PathVariable Long accountId) throws AccountNotFoundException {
         return accountService.getAccount(accountId);
     }
 }
--- src/main/java/com/tuum/bankassignment/controller/TransactionController.java
+++ src/main/java/com/tuum/bankassignment/controller/TransactionController.java
@@ -2,20 +2,32 @@ package com.tuum.bankassignment.controller;
 
 import com.tuum.bankassignment.dto.CreateTransactionDTO;
 import com.tuum.bankassignment.entity.Transaction;
+import com.tuum.bankassignment.exception.AccountNotFoundException;
+import com.tuum.bankassignment.service.AccountService;
 import com.tuum.bankassignment.service.TransactionService;
-import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("transaction")
 public class TransactionController {
 
     @Autowired
     TransactionService transactionService;
+    @Autowired
+    AccountService accountService;
 
-    @PostMapping("/transaction")
-    Transaction createTransaction(@RequestBody CreateTransactionDTO transaction) throws NotFoundException {
+    @PostMapping
+    Transaction createTransaction(@RequestBody CreateTransactionDTO transaction) throws AccountNotFoundException {
         return transactionService.createTransaction(transaction);
+    }
+    @GetMapping("{accountId}/all")
+    List<Transaction> getTransactions(
+            @PathVariable("accountId") Long accountId,
+            @RequestParam(value = "page", defaultValue = "0") Short page,
+            @RequestParam(value = "size", defaultValue = "10") Short size){
+        return accountService.getTransactions(accountId, page, size);
     }
 }
