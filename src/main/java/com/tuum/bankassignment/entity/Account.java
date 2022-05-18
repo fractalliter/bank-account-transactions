5f7bc686107c16aeb86831b994ea8d7022e67c41
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-18 20:51:43 +0430

    add constructor

--- src/main/java/com/tuum/bankassignment/entity/Account.java
+++ src/main/java/com/tuum/bankassignment/entity/Account.java
@@ -3,16 +3,25 @@ package com.tuum.bankassignment.entity;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Account implements Serializable {
     private Long accountId;
     private String customerId;
     private String country;
+
+    private Set<Currency> currencies;
     private List<Balance> balances = new LinkedList<>();
 
     public Account(String customerId, String country) {
         this.customerId = customerId;
         this.country = country;
+    }
+
+    public Account(String customerId, String country, Set<Currency> currencies) {
+        this.customerId = customerId;
+        this.country = country;
+        this.currencies = currencies;
     }
 
     public Account(String customerId, String country, List<Balance> balances) {
@@ -24,6 +33,14 @@ public class Account implements Serializable {
 
     public Account() {
         super();
+    }
+
+    public Set<Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Set<Currency> currencies) {
+        this.currencies = currencies;
     }
 
     public String getCustomerId() {
