578a8a62f0b4b840a7c0bd093f2435c9abda4c11
elias <eliass.rahmani@gmail.com>
2024-03-16 13:47:57 +0100

    improve logic

--- src/main/java/com/earl/bank/service/AccountServiceImpl.java
+++ src/main/java/com/earl/bank/service/AccountServiceImpl.java
@@ -3,10 +3,8 @@ package com.earl.bank.service;
 import com.earl.bank.dto.CreateAccountDTO;
 import com.earl.bank.entity.Account;
 import com.earl.bank.entity.Balance;
-import com.earl.bank.entity.Currency;
 import com.earl.bank.entity.Transaction;
 import com.earl.bank.exception.AccountNotFoundException;
-import com.earl.bank.exception.InvalidCurrencyException;
 import com.earl.bank.mapper.AccountMapper;
 import com.earl.bank.mapper.BalanceMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +13,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,19 +30,15 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     @Transactional(isolation = Isolation.REPEATABLE_READ)
-    public Account createAccount(CreateAccountDTO account) throws InvalidCurrencyException {
-        var tempCurrency = new HashSet<>(Set.copyOf(account.getCurrency()));
-        tempCurrency.removeAll(Arrays.stream(Currency.values()).collect(Collectors.toSet()));
-        if (account.getCurrency().size() == 0 || tempCurrency.size() != 0) {
-            throw new InvalidCurrencyException();
-        }
+    public Account createAccount(CreateAccountDTO account) {
         var newAccount = new Account(account.getCustomerId(), account.getCountry());
-        accountMapper.createAccount(newAccount);
-        account.getCurrency().forEach(currency -> {
-            var balance = new Balance(newAccount.getAccountId(), BigDecimal.ZERO, currency);
-            balanceMapper.createBalance(balance);
-        });
-        return getAccount(newAccount.getAccountId());
+        var persistedAccount = accountMapper.createAccount(newAccount);
+        List<Balance> balances = account.getCurrency().stream().map(currency -> {
+            var balance = new Balance(persistedAccount.getAccountId(), BigDecimal.ZERO, currency);
+            return balanceMapper.createBalance(balance);
+        }).collect(Collectors.toList());
+        persistedAccount.setBalances(balances);
+        return persistedAccount;
     }
 
     @Override
