7ffbb689afe331157802a74a07c4b120b2558380
elias <eliass.rahmani@gmail.com>
2024-03-16 13:49:44 +0100

    introduce a strategy pattern

--- src/main/java/com/earl/bank/entity/Direction.java
+++ src/main/java/com/earl/bank/entity/Direction.java
@@ -1,5 +1,37 @@
 package com.earl.bank.entity;
 
+import com.earl.bank.dto.CreateTransactionDTO;
+import com.earl.bank.exception.InsufficientFundException;
+import com.earl.bank.mapper.BalanceMapper;
+
+import java.math.BigDecimal;
+
 public enum Direction {
-    IN, OUT
+    IN {
+        @Override
+        public Balance handler(BalanceMapper balanceMapper, Account account, CreateTransactionDTO transaction) {
+            return balanceMapper.increaseBalance(
+                    account.getAccountId(),
+                    transaction.getCurrency(),
+                    transaction.getAmount()
+            );
+        }
+    }, OUT {
+        @Override
+        public Balance handler(BalanceMapper balanceMapper, Account account, CreateTransactionDTO transaction) {
+            var balanceComparison = balanceMapper.getBalance(
+                    transaction.getAccountId(),
+                    transaction.getCurrency()
+            ).getAmount().subtract(transaction.getAmount()).compareTo(BigDecimal.ZERO);
+            if (balanceComparison < 0)
+                throw new InsufficientFundException();
+            return balanceMapper.decreaseBalance(
+                    account.getAccountId(),
+                    transaction.getCurrency(),
+                    transaction.getAmount()
+            );
+        }
+    };
+
+    public abstract Balance handler(BalanceMapper balanceMapper, Account account, CreateTransactionDTO transaction);
 }
