0a8c22449b197257b97bb81dcb6686bdda020cfb
elias <eliass.rahmani@gmail.com>
2024-03-16 13:49:19 +0100

    enhance DTOs

--- src/main/java/com/earl/bank/dto/CreateAccountDTO.java
+++ src/main/java/com/earl/bank/dto/CreateAccountDTO.java
@@ -2,11 +2,19 @@ package com.earl.bank.dto;
 
 import com.earl.bank.entity.Currency;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 public class CreateAccountDTO {
+
+    @NotBlank(message = "Customer Id is required")
     String customerId;
-    String Country;
+
+    @NotBlank(message = "Country is required")
+    String country;
+
+    @NotEmpty(message = "At least one currency should be in the list")
     Set<Currency> currency;
 
     public String getCustomerId() {
@@ -18,11 +26,11 @@ public class CreateAccountDTO {
     }
 
     public String getCountry() {
-        return Country;
+        return country;
     }
 
     public void setCountry(String country) {
-        Country = country;
+        this.country = country;
     }
 
     public Set<Currency> getCurrency() {
