8570413f7a25f20e6d5231a6860ae2b0acc67fd3
elias <eliass.rahmani@gmail.com>
2024-03-17 00:45:44 +0100

    improve transaction service tests

--- src/test/java/com/earl/bank/service/TransactionServiceTest.java
+++ src/test/java/com/earl/bank/service/TransactionServiceTest.java
@@ -2,8 +2,7 @@ package com.earl.bank.service;
 
 import com.earl.bank.dto.CreateAccountDTO;
 import com.earl.bank.dto.CreateTransactionDTO;
-import com.earl.bank.entity.Currency;
-import com.earl.bank.entity.Direction;
+import com.earl.bank.entity.*;
 import com.earl.bank.exception.AccountNotFoundException;
 import com.earl.bank.exception.InsufficientFundException;
 import com.earl.bank.exception.InvalidAmountException;
@@ -13,10 +12,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -29,25 +31,29 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class TransactionServiceTest {
-
-    @MockBean
+    @Autowired
     TransactionService transactionService;
-
-    @Autowired
-    TransactionMapper transactionMapper;
-
-    @Autowired
-    BalanceMapper balanceMapper;
-    @Autowired
+    @Mock
     AccountService accountService;
+    @Mock
+    TransactionMapper transactionMapper;
+    @Mock
+    BalanceMapper balanceMapper;
 
     @BeforeEach
     void setUp() {
-        transactionService = new TransactionServiceImpl(transactionMapper, balanceMapper, accountService);
+        transactionService = new TransactionServiceImpl(
+                transactionMapper,
+                balanceMapper,
+                accountService
+        );
     }
 
     @AfterEach
@@ -67,60 +73,98 @@ class TransactionServiceTest {
     @EnumSource(Currency.class)
     @DisplayName("Transaction deposits money into user account")
     void createTransactionDeposit(Currency currency) {
-        var accountDTO = new CreateAccountDTO();
-        accountDTO.setCountry("Iran");
-        accountDTO.setCurrency(Set.of(Currency.values()));
-        accountDTO.setCustomerId("1234");
-        var account = accountService.createAccount(accountDTO);
-
-        assertNotNull(account);
-
+        // Given
+        var account = new Account("1234", "Iran");
+        account.setAccountId(1L);
         var transactionDTO = new CreateTransactionDTO();
         transactionDTO.setAccountId(account.getAccountId());
         transactionDTO.setAmount(new BigDecimal("1000.00"));
         transactionDTO.setCurrency(currency);
         transactionDTO.setDescription("test");
         transactionDTO.setDirection(Direction.IN);
-        var transaction = transactionService.createTransaction(transactionDTO);
+        var newTransaction = new Transaction(
+                transactionDTO.getAccountId(),
+                transactionDTO.getAmount(),
+                transactionDTO.getCurrency(),
+                transactionDTO.getDirection(),
+                transactionDTO.getDescription()
+        );
+        var newBalance = new Balance(
+                account.getAccountId(),
+                new BigDecimal("1010.00"),
+                currency);
 
+        // When
+        when(accountService.getAccount(1L)).thenReturn(account);
+        when(balanceMapper
+                .increaseBalance(
+                        transactionDTO.getAccountId(),
+                        transactionDTO.getCurrency(),
+                        transactionDTO.getAmount())).thenReturn(newBalance);
+        when(transactionMapper.createTransaction(any(Transaction.class))).thenReturn(newTransaction);
+
+
+        // Then
+        var transaction = transactionService.createTransaction(transactionDTO);
         assertNotNull(transaction);
         assertNotNull(transaction.getBalance());
-        Assertions.assertEquals(transaction.getBalance().getAmount().compareTo(new BigDecimal("1000.00")), 0);
+        Assertions.assertEquals(0,
+                transaction.getBalance().getAmount().compareTo(new BigDecimal("1010.00")));
         assertEquals(transaction.getAccountId(), account.getAccountId());
         Assertions.assertEquals(transaction.getBalance().getCurrency(), currency);
-        Assertions.assertEquals(transaction.getDirection(), Direction.IN);
+        Assertions.assertEquals(Direction.IN, transaction.getDirection());
     }
 
     @ParameterizedTest(name = "for currency {0}")
     @EnumSource(Currency.class)
     @DisplayName("transaction withdraws money from user account")
     void createTransactionWithdraw(Currency currency) {
-        var accountDTO = new CreateAccountDTO();
-        accountDTO.setCountry("Iran");
-        accountDTO.setCurrency(Set.of(Currency.values()));
-        accountDTO.setCustomerId("1234");
-        var account = accountService.createAccount(accountDTO);
-        assertNotNull(account);
-
+        // Given
+        var account = new Account("1234", "Iran");
+        account.setAccountId(1L);
         var transactionDTO = new CreateTransactionDTO();
         transactionDTO.setAccountId(account.getAccountId());
         transactionDTO.setAmount(new BigDecimal("100.00"));
         transactionDTO.setCurrency(currency);
         transactionDTO.setDescription("test");
-        transactionDTO.setDirection(Direction.IN);
+        transactionDTO.setDirection(Direction.OUT);
+        var newTransaction = new Transaction(
+                transactionDTO.getAccountId(),
+                transactionDTO.getAmount(),
+                transactionDTO.getCurrency(),
+                transactionDTO.getDirection(),
+                transactionDTO.getDescription()
+        );
+        var currentBalance = new Balance(
+                account.getAccountId(),
+                new BigDecimal("1000.00"),
+                currency);
+        var newBalance = new Balance(
+                account.getAccountId(),
+                new BigDecimal("900.00"),
+                currency);
+
+        // When
+        when(accountService.getAccount(account.getAccountId())).thenReturn(account);
+        when(balanceMapper.getBalance(account.getAccountId(), transactionDTO.getCurrency()))
+                .thenReturn(currentBalance);
+        when(balanceMapper
+                .decreaseBalance(
+                        account.getAccountId(),
+                        transactionDTO.getCurrency(),
+                        transactionDTO.getAmount())).thenReturn(newBalance);
+        when(transactionMapper.createTransaction(any(Transaction.class))).thenReturn(newTransaction);
+
+
+        // Then
         var transaction = transactionService.createTransaction(transactionDTO);
         assertNotNull(transaction);
-
-        transactionDTO.setAmount(new BigDecimal("10.00"));
-        transactionDTO.setDirection(Direction.OUT);
-        transaction = transactionService.createTransaction(transactionDTO);
-
-        assertNotNull(transaction);
         assertNotNull(transaction.getBalance());
-        Assertions.assertEquals(transaction.getBalance().getAmount().compareTo(new BigDecimal("90.00")), 0);
+        Assertions.assertEquals(0,
+                transaction.getBalance().getAmount().compareTo(new BigDecimal("900.00")));
         assertEquals(transaction.getAccountId(), account.getAccountId());
         Assertions.assertEquals(transaction.getBalance().getCurrency(), currency);
-        Assertions.assertEquals(transaction.getDirection(), Direction.OUT);
+        Assertions.assertEquals(Direction.OUT, transaction.getDirection());
     }
 
     @ParameterizedTest(name = "for currency {0} and direction {1}")
